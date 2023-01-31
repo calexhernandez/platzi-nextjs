96752cbb559348cb3b85e614627106ea2f009aea
Cesar Hernandez <cesar.lopex@gmail.com>
2023-01-30 21:01:48 -0600

    p1

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar"
+
+
+const About = () => {
+
+    return (
+        <div>
+            <Navbar /> 
+            <p>Esta es la pagina de about</p>
+        </div>
+    )
+    
+    
+}
+
+export default About;
